0ad0dda64e85d60a9cb6e28ef5f9c2128282a9d0
shahbazhashmi14a <shahbazhashmi14a@gmail.com>
2024-10-03 13:24:34 +0000

    Day 2 of 30 days Challange by CES SIT

---
+++ var_init.c
@@ -0,0 +1,12 @@
+//Day 2 of 30 days Challange by CES SIT
+//Practice declaring and Initializing Variables (INT, FLOAT, CHAR).
+
+#include <stdio.h>
+void main() {
+    int A = 12;
+    float B = 23.43;
+    char C = 'A';
+    printf("A = %d \n", A);
+    printf("B = %f \n", B);
+    printf("C = %c \n", C);
+}
