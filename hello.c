c7994a992c85462c08241a110c7bfeb81ff40baa
shahbazhashmi14a <shahbazhashmi14a@gmail.com>
2024-10-03 13:26:16 +0000

    Day 1 of 30 days Challange by CES SIT

--- hello.c
+++ hello.c
@@ -1,7 +1,9 @@
 //Day 1 of 30 days Challange by CES SIT
+//Write a program to print "Hello World !!"
+
 #include <stdio.h>
 
 int main() {
-    printf("Hello World! \n");
+    printf("Hello World !! \n");
     return 0;
 }
