9447a48bc0b23bb8c4e7c835306ca3433a629876
shahbazhashmi14a <shahbazhashmi14a@gmail.com>
2024-11-09 10:12:38 +0000

    Day 14 of 30 days Challange by CES SIT

---
+++ prime.c
@@ -0,0 +1,32 @@
+// Day 14 of 30 days Challange by CES SIT
+// IMPLEMENT A PROGRAM TO CHECK IF A NUMBER IS PRIME.
+
+#include <stdio.h>
+#include <stdbool.h>
+
+bool isPrime(int num) {
+    if (num <= 1) {
+        return false;
+    }
+    for (int i = 2; i <= num / 2; i++) {
+        if (num % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    int num;
+
+    printf("Enter a positive integer: ");
+    scanf("%d", &num);
+
+    if (isPrime(num)) {
+        printf("%d is a prime number.\n", num);
+    } else {
+        printf("%d is not a prime number.\n", num);
+    }
+
+    return 0;
+}
